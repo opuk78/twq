5e6a427ecaee14360e3e6c9c83a952c04f5c1023
opuk78 <94242571+opuk78@users.noreply.github.com>
2026-03-12 08:58:23 +0100

    Update guests.js

--- guests.js
+++ guests.js
@@ -1,1 +1,7 @@
-
+const guestRows = [
+  { room: 'Room N1', guest: 'Georgios' },
+  { room: 'Room N2', guest: 'Samantha' },
+  { room: 'Room N3', guest: 'Andrej' },
+  { room: 'Room N4', guest: 'Caroline' },
+  { room: 'Prinsezzin', guest: '' }
+];
